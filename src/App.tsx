76da0ec73493821815f55943b0b882bd4febfa67
PlanC90 <167799741+PlanC90@users.noreply.github.com>
2025-05-20 17:55:42 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -107,7 +107,7 @@ function App() {
 
           {/* Holder Count */}
           <div className="text-6xl font-extrabold text-blue-400 mb-8">
-            580,000+ Holders🔥
+            600,000+ Holders🔥
           </div>
 
           <div className="flex flex-col sm:flex-row gap-4 mb-16">
