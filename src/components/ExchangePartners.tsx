e6169010845fe985bd5c82a15137f8684fbd1e9e
PlanC90 <167799741+PlanC90@users.noreply.github.com>
2025-07-15 14:50:16 +0300

    Initial commit

--- src/components/ExchangePartners.tsx
+++ src/components/ExchangePartners.tsx
@@ -106,6 +106,46 @@ const ExchangePartners = () => {
               className="w-32 h-auto"
             />
           </a>
+          {/* CoinCodex */}
+          <a href="https://coincodex.com/crypto/memexsol/" target="_blank" rel="noopener noreferrer">
+            <img
+              src="https://pbs.twimg.com/profile_images/1922619676497944576/Fsiv6gE2_400x400.jpg"
+              alt="CoinCodex"
+              className="w-32 h-auto"
+            />
+          </a>
+           {/* Kriptomat */}
+          <a href="https://kriptomat.io/cryptocurrency-prices/memexsol-price/" target="_blank" rel="noopener noreferrer">
+            <img
+              src="https://pbs.twimg.com/profile_images/1724063342938005504/uqSC-UD2_400x400.jpg"
+              alt="Kriptomat"
+              className="w-32 h-auto"
+            />
+          </a>
+          {/* CoinStats */}
+          <a href="https://coinstats.app/tr/coins/memexsol/" target="_blank" rel="noopener noreferrer">
+            <img
+              src="https://pbs.twimg.com/profile_images/1944729554074959872/7f2x1AFs_400x400.jpg"
+              alt="CoinStats"
+              className="w-32 h-auto"
+            />
+          </a>
+          {/* Pump.fun */}
+          <a href="https://swap.pump.fun/?input=So11111111111111111111111111111111111111112&output=AsjP9VyKUSuLSeycoTb9AqGs3PH6BWAvqDoKmxrWpump" target="_blank" rel="noopener noreferrer">
+            <img
+              src="https://pbs.twimg.com/profile_images/1748108741503803392/EmT4yP6S_400x400.jpg"
+              alt="Pump.fun"
+              className="w-32 h-auto"
+            />
+          </a>
+          {/* Dexscreener */}
+          <a href="https://dexscreener.com/solana/5fxe95ubfy5kbacgr59zbtcczage5wgaaedheblblhr4" target="_blank" rel="noopener noreferrer">
+            <img
+              src="https://pbs.twimg.com/profile_images/1462287879565201409/5UYqudVs_400x400.jpg"
+              alt="Dexscreener"
+              className="w-32 h-auto"
+            />
+          </a>
         </div>
       </div>
     </section>
